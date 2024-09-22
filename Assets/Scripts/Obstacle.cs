a59a305e29f84d3bf68c23a21ffe283f96f0946e
Cosinus215 <Cosinus215@gmail.com>
2024-09-23 01:16:40 +0200

    Add stairs obstacle

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
+    [SerializeField] private int damage;
 
     private void Update() {
         transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y);
@@ -8,7 +9,7 @@ public class Obstacle : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.TryGetComponent(out IDamageable iDamage)) {
-            iDamage.Damage(1);
+            iDamage.Damage(damage);
         }
     }
 }
