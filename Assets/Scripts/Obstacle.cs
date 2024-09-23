55c941aaaace02e046d040911d65e8ec288c40b8
Cosinus215 <Cosinus215@gmail.com>
2024-09-23 04:33:16 +0200

    Add player jump and movement

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour {
     [SerializeField] private int damage;
 
-    private void Update() {
-        transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y);
+    private void FixedUpdate() {
+        transform.position = new Vector2(transform.position.x - 0.06f, transform.position.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, IDamageable {
-    public int health;
+    [SerializeField] private int health;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpingForce;
+    private Vector2 movementVector;
+    private Rigidbody2D rb;
+    private bool isJumping; 
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        rb.velocity = new Vector2(movementVector.x * speed, rb.velocity.y);
+
+        if (isJumping && Mathf.Abs(rb.velocity.y) < 0.01f) {
+            rb.AddForce(Vector2.up * jumpingForce, ForceMode2D.Impulse);
+        }
+    }
 
     public void Damage(int value) {
         health -= value;
         if (health < 1) {
             Destroy(gameObject);
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        movementVector = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (context.performed) {
+            isJumping = true;
+        }
+
+        if (context.canceled) {
+            isJumping = false;
         }
     }
 }
