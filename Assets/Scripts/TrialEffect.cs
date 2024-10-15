47967431072901c0713e2b071b99c73dc0c465a9
Cosinus215 <Cosinus215@gmail.com>
2024-10-15 11:34:08 +0200

    Add Player changing color trial effect

--- Assets/Scripts/TrialEffect.cs
+++ Assets/Scripts/TrialEffect.cs
@@ -1,12 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TrialEffect : MonoBehaviour {
-    private LineRenderer lineRenderer;
-    private Player player;
+    [SerializeField] private SpriteRenderer playerBackground;
+    [SerializeField] private float colorSwitchSpeed;
+    [SerializeField] private float alphaEnd;
     [SerializeField] private float moveSpeedNotMoving;
     [SerializeField] private float moveSpeedMoving;
     [SerializeField] private float comeBackSpeed;
+    private LineRenderer lineRenderer;
+    private Player player;
+    private Color lineColor;
+    private float color;
 
     private void Awake() {
         GetComponents();
@@ -14,12 +20,46 @@ public class TrialEffect : MonoBehaviour {
 
     private void Start() {
         StartCoroutine(MakeTrial());
+        StartCoroutine(ChangeTrailColors());
     }
 
     private void GetComponents() {
         lineRenderer = GetComponent<LineRenderer>();
         player = GetComponent<Player>();
     }
+
+    private void CreateGradientKeys(out GradientAlphaKey[] gradientAlphas,
+        out List<GradientColorKey> gradientColors) {
+        GradientAlphaKey startAlpha = new(1, 0.0f);
+        GradientAlphaKey endAlpha = new(alphaEnd / 255, 1.0f);
+        gradientAlphas = new[]{ startAlpha, endAlpha };
+
+        gradientColors = new();
+    }
+
+    private IEnumerator ChangeTrailColors() {
+        Gradient gradient = new();
+        CreateGradientKeys(
+            out GradientAlphaKey[] gradientAlphas,
+            out List<GradientColorKey> gradientColors
+        );
+
+        while (true) {
+            color = (color + colorSwitchSpeed * Time.deltaTime) % 1f;
+            lineColor = Color.HSVToRGB(color, 1f, 1f);
+            playerBackground.color = lineColor;
+
+            GradientColorKey trialColor = new(lineColor, 0.0f);
+            gradientColors.Clear();
+            gradientColors.Add(trialColor);
+
+            gradient.SetKeys(gradientColors.ToArray(), gradientAlphas);
+
+            lineRenderer.colorGradient = gradient;
+            yield return null;
+        }
+    }
+
 
     private IEnumerator MakeTrial() {
         Vector3 moveVector;
@@ -33,7 +73,7 @@ public class TrialEffect : MonoBehaviour {
             if (player.movementVector == Vector2.zero) {
                 moveVector = new Vector3(moveSpeedNotMoving, 0, 0);
             }
-            
+
             lineRenderer.SetPosition(lineRenderer.positionCount - 1, playerPos);
             for (int i = lineRenderer.positionCount - 2; i >= 0; i--) {
                 Vector3 targetPos = lineRenderer.GetPosition(i + 1) - moveVector;
